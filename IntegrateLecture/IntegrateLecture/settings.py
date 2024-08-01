3c44450c591417f92a8c6848690341f52a6c6675
SeungGURY <84478606+Kim-2301@users.noreply.github.com>
2024-08-01 13:27:41 +0900

    Revert "Feature/hyun"

--- IntegrateLecture/IntegrateLecture/settings.py
+++ IntegrateLecture/IntegrateLecture/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import environ
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,8 +42,6 @@ INSTALLED_APPS = [
     'lecture',
     'drf_yasg',
     'rest_framework',
-    "django_filters",
-
 ]
 
 MIDDLEWARE = [
