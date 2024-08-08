362950e15819cea554d022b8768f49b69e1f0825
김승훈 <eephus625@icloud.com>
2024-08-08 14:32:51 +0900

    feat: 하트 버튼을 눌렀을 때 강의를 찜할 수 있는 기능 추가

--- IntegrateLecture/lecture/urls.py
+++ IntegrateLecture/lecture/urls.py
@@ -4,25 +4,31 @@ from .views import *
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('lecture/detail/<str:pk>/', LectureDetailTemplateView.as_view(), name='lecture_detail'),
+    path(
+        "lecture/detail/<str:pk>/",
+        LectureDetailTemplateView.as_view(),
+        name="lecture_detail",
+    ),
     path("main/", LectureListPageView.as_view(), name="lecture_list_page"),
-
-    path('api/detail/<str:pk>/', LectureDetailView.as_view(), name='lecture_detail'),
+    path("api/detail/<str:pk>/", LectureDetailView.as_view(), name="lecture_detail"),
     path("api/lecture/", LectureListView.as_view(), name="lecture_list_api"),
     path("api/categories/", CategoryListView.as_view(), name="category_list_api"),
-
-    path("index/", LectureListPageView.as_view(), name='index'),
-
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
-    path('user/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
-    path('user/<int:pk>/wishlist/', WishListView.as_view(), name='user_wishlist'),
-    path('user/<int:pk>/wishlist/add/', WishListCreateView.as_view(), name='wishlist_add'),
-
-    path('api/signup/', APIUserSignupView.as_view(), name='user_signup_api'),
-    path('api/users/', APIUserListView.as_view(), name='user_list_api'),
-    path('api/users/<str:pk>', APIUserDetailView.as_view(), name='user_detail_api')
-
+    path("index/", LectureListPageView.as_view(), name="index"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("user/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
+    path("user/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
+    path("user/<int:pk>/wishlist/", WishListView.as_view(), name="user_wishlist"),
+    path(
+        "user/<int:pk>/wishlist/add/", WishListCreateView.as_view(), name="wishlist_add"
+    ),
+    path(
+        "user/<int:pk>/wishlist/remove/",
+        WishListRemoveView.as_view(),
+        name="wishlist_remove",
+    ),
+    path("api/signup/", APIUserSignupView.as_view(), name="user_signup_api"),
+    path("api/users/", APIUserListView.as_view(), name="user_list_api"),
+    path("api/users/<str:pk>", APIUserDetailView.as_view(), name="user_detail_api"),
 ]
