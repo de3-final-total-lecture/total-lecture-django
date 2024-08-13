67ec8dde2764b4253db6bc15ea6438c5b72d728c
kdk0411 <99461483+kdk0411@users.noreply.github.com>
2024-08-13 21:35:43 +0900

    feat: User Click, Search, Tag Click Event 처리 기능 추가

--- IntegrateLecture/lecture/urls.py
+++ IntegrateLecture/lecture/urls.py
@@ -10,34 +10,17 @@ urlpatterns = [
     path("api/lecture/", LectureListView.as_view(), name="lecture_list_api"),
     path("api/categories/", CategoryListView.as_view(), name="category_list_api"),
     path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("api/logout/", LogoutView.as_view(), name="logout"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("user/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
     path("user/<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
-    path("user/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
+    path("api/user/<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
     path("user/<int:pk>/wishlist/", WishListView.as_view(), name="user_wishlist"),
-    path(
-        "user/<int:pk>/wishlist/add/", WishListCreateView.as_view(), name="wishlist_add"
-    ),
-    path(
-        "user/<int:pk>/wishlist/remove/",
-        WishListRemoveView.as_view(),
-        name="wishlist_remove",
-    ),
-    path(
-        "wishlist/status/<str:lecture_id>/",
-        WishListStatusView.as_view(),
-        name="wishlist_status",
-    ),
-    path("user_click", ClickEventView.as_view(), name="user_click")
-    path("tag_click/", TagClickEventView.as_view(), name="tag_click"),
-    path("user_search/", SearchEventView.as_view(), name="user_search"),
-    path(
-        "wishlist/toggle_alarm/<str:lecture_id>/",
-        ToggleAlarmView.as_view(),
-        name="toggle_alarm",
-    ),
-    path("api/signup/", APIUserSignupView.as_view(), name="user_signup_api"),
-    path("api/users/", APIUserListView.as_view(), name="user_list_api"),
-    path("api/users/<str:pk>", APIUserDetailView.as_view(), name="user_detail_api"),
+    path("user/<int:pk>/wishlist/add/", WishListCreateView.as_view(), name="wishlist_add"),
+    path("user/<int:pk>/wishlist/remove/", WishListRemoveView.as_view(), name="wishlist_remove"),
+    path("wishlist/status/<str:lecture_id>/", WishListStatusView.as_view(), name="wishlist_status"),
+    path("api/user_click", ClickEventView.as_view(), name="user_click"),
+    path("api/tag_click/", TagClickEventView.as_view(), name="tag_click"),
+    path("api/user_search/", SearchEventView.as_view(), name="user_search"),
+    path("wishlist/toggle_alarm/<str:lecture_id>/", ToggleAlarmView.as_view(), name="toggle_alarm"),
 ]
